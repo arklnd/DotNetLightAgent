bd2a5167fc5a3084f0094c8ac5bee0b6a74c41b4
Arijit Kundu <Arijit.Kundu@hyland.com>
2025-07-23 12:40:16 +0530

    fix: update ShutdownTimeout values for StdioClientTransportOptions to 90 seconds

--- src/DotNetLightAgent/Models/MCPList.cs
+++ src/DotNetLightAgent/Models/MCPList.cs
@@ -14,42 +14,42 @@ namespace DotNetLightAgent.Models
                 Name = "FileSystem",
                 Command = "npx",
                 Arguments = ["-y", "@modelcontextprotocol/server-filesystem", "C:\\ARIJIT"],
-                ShutdownTimeout = TimeSpan.FromSeconds(10)
+                ShutdownTimeout = TimeSpan.FromSeconds(90)
             },
             new StdioClientTransportOptions
             {
                 Name = "Sequential_Thinker",
                 Command = "npx",
                 Arguments = ["-y", "@modelcontextprotocol/server-sequential-thinking"],
-                ShutdownTimeout = TimeSpan.FromSeconds(10)
+                ShutdownTimeout = TimeSpan.FromSeconds(90)
             },
             new StdioClientTransportOptions
             {
                 Name = "fetch_web",
                 Command = "python",
                 Arguments = ["-m", "mcp_server_fetch"],
-                ShutdownTimeout = TimeSpan.FromSeconds(10)
+                ShutdownTimeout = TimeSpan.FromSeconds(90)
             },
             new StdioClientTransportOptions
             {
                 Name = "git",
                 Command = "python",
                 Arguments = ["-m", "mcp_server_git"],
-                ShutdownTimeout = TimeSpan.FromSeconds(10)
+                ShutdownTimeout = TimeSpan.FromSeconds(90)
             },
             new StdioClientTransportOptions
             {
                 Name = "time",
                 Command = "python",
                 Arguments = ["-m", "mcp_server_time"],
-                ShutdownTimeout = TimeSpan.FromSeconds(10)
+                ShutdownTimeout = TimeSpan.FromSeconds(90)
             },
             new StdioClientTransportOptions
             {
                 Name = "playwright",
                 Command = "npx",
                 Arguments = ["@playwright/mcp@latest"],
-                ShutdownTimeout = TimeSpan.FromSeconds(10)
+                ShutdownTimeout = TimeSpan.FromSeconds(90)
             },
         ];
     }
